c05c27dae418885a0ce8e8ca755d88217fbce50f
Ivan <ivkosarev@gmail.com>
2021-07-07 11:26:52 +0000

    add tfvars example

--- terraform/install_docker_compose.sh
+++ terraform/install_docker_compose.sh
@@ -12,4 +12,4 @@ sudo docker -v
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
