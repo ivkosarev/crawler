c0f4bbc445b80d4fd9d5bb3d1be2f45364c36f7f
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-07-08 07:06:21 +0400

    file docker/docker-compose.yml changed

---
+++ src/crawler/start_crawler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker pull mongo:latest
+docker pull rabbitmq:3-management-alpine
+echo `git show --format="%h" HEAD | head -1` > build_info.txt
+echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
+export USER_NAME=mzabolotnov
+docker build -t $USER_NAME/crawler:1.0 .
+docker network create backnet
+docker volume create mongo_db
+docker volume create rabbit
+docker run --rm -d --network=backnet --network-alias=crawler_db -v mongo_db:/data/db mongo:latest
+docker run --rm -d --network=backnet --network-alias=rabbitmq -v rabbit:/var rabbitmq:3-management-alpine
+docker run --rm -d --network=backnet --network-alias=crawler $USER_NAME/crawler:1.0
