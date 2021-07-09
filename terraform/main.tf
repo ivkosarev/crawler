27113c0bb7b3f4144b56237d3f0794323af15615
Ivan <ivkosarev@gmail.com>
2021-07-09 13:39:26 +0000

    наводим чистоту в репе

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,13 +35,12 @@ resource "yandex_compute_instance" "app" {
   host = yandex_compute_instance.app[count.index].network_interface.0.nat_ip_address
   #host = yandex_compute_instance.app.network_interface.0.nat_ip_address
   user = "ubuntu"
-  #password = var.password
+  password = var.password
   agent = false
   # путь до приватного ключа
   private_key = file("./usr1")
   }
   
-
   provisioner "file" {
    source = "./install_docker_compose.sh"
    destination = "/home/ubuntu/install_docker_compose.sh"
@@ -51,6 +50,5 @@ resource "yandex_compute_instance" "app" {
     "sudo bash /home/ubuntu/install_docker_compose.sh"
   ]
   }
-
   
 }
